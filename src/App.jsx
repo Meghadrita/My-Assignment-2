33a34b9b3bfe4c52807de37346d03e434555ebc0
Meghadrita <mahiyatabassumsmita@gmail.com>
2026-03-05 20:05:30 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Suspense } from 'react'
 import './App.css'
+import Card from './Components/Card/Card'
+import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer/Footer'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const fetchCardData = async () => {
+    const res = await fetch("/card_data.json")
+    return res.json();
+  }
+
+  const cardDatapromise = fetchCardData();
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <NavBar></NavBar>
+      <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+        <Card cardDatapromise={cardDatapromise}></Card>
+      </Suspense>
+      <Footer></Footer>
+
+
+      <ToastContainer></ToastContainer>
     </>
   )
 }
---
+++ src/Component/NavBar/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const NavBar = () => {
+	return (
+		<div className="navbar bg-base-100 shadow-sm w-full md:max-w-[1200px] mx-auto px-4">
+
+
+			<div className="flex-1">
+				<a className="btn btn-ghost text-xl">CS — Ticket System</a>
+			</div>
+
+
+			<div className="hidden md:flex gap-5 items-center">
+				<button>Home</button>
+				<button>FAQ</button>
+				<button>Changelog</button>
+				<button>Blog</button>
+				<button>Download</button>
+				<button>Contact</button>
+				<button className="p-2 rounded-xl text-white bg-[linear-gradient(to_bottom_right,#6D28D9,#7C3AED,#9333EA)]">
+					+ New Ticket
+				</button>
+
+
+			</div>
+
+
+			<div className="md:hidden dropdown dropdown-end">
+				<label tabIndex={0} className="btn btn-ghost text-xl">
+					⋮
+				</label>
+
+				<ul
+					tabIndex={0}
+					className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40"
+				>
+					<li><button>Home</button></li>
+					<li><button>FAQ</button></li>
+					<li><button>Changelog</button></li>
+					<li><button>Blog</button></li>
+					<li><button>Download</button></li>
+					<li><button>Contact</button></li>
+					<button className="p-2 rounded-xl text-white bg-[linear-gradient(to_bottom_right,#6D28D9,#7C3AED,#9333EA)]">
+						+ New Ticket
+					</button>
+				</ul>
+			</div>
+
+		</div>
+	);
+};
+
+export default NavBar;
